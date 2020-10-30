7b010c15b3a4e05ecaeed0ad07e486aa5450af33
Simon Heiler <simon.julian.heiler@gmail.com>
2020-10-30 14:12:01 +0100

    Some work on WAF.

--- src/data_management/get_figures.py
+++ src/data_management/get_figures.py
@@ -2,7 +2,6 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import requests
 
 from bld.project_paths import project_paths_join as ppj
 
@@ -28,6 +27,7 @@ def plot_development_over_time(input_data):
             input_data["positive_rate_14d"],
         ],
     )
+    iso_code = input_data.iso_code.unique().item()
 
     fig, ax = plt.subplots(nrows=3, ncols=3, figsize=(15, 15))
 
@@ -58,7 +58,7 @@ def plot_development_over_time(input_data):
             ax[row, col].set_xlim((plot_x[0], plot_x[-1]))
             plt.setp(ax[row, col].xaxis.get_majorticklabels(), rotation=90)
 
-    plt.show()
+    fig.savefig(ppj("OUT_FIGURES", iso_code + ".pdf"))
 
 
 #######################
@@ -70,46 +70,46 @@ if __name__ == "__main__":
     idx = pd.IndexSlice
 
     owid_dtypes = {
-        'iso_code': str,
-        'continent': str,
-        'location': str,
-        'total_cases': float,
-        'new_cases': float,
-        'new_cases_smoothed': float,
-        'total_deaths': float,
-        'new_deaths': float,
-        'new_deaths_smoothed': float,
-        'total_cases_per_million': float,
-        'new_cases_per_million': float,
-        'new_cases_smoothed_per_million': float,
-        'total_deaths_per_million': float,
-        'new_deaths_per_million': float,
-        'new_deaths_smoothed_per_million': float,
-        'total_tests': float,
-        'new_tests': float,
-        'total_tests_per_thousand': float,
-        'new_tests_per_thousand': float,
-        'new_tests_smoothed': float,
-        'new_tests_smoothed_per_thousand': float,
-        'tests_per_case': float,
-        'positive_rate': float,
-        'tests_units': str,
-        'stringency_index': float,
-        'population': float,
-        'population_density': float,
-        'median_age': float,
-        'aged_65_older': float,
-        'aged_70_older': float,
-        'gdp_per_capita': float,
-        'extreme_poverty': float,
-        'cardiovasc_death_rate': float,
-        'diabetes_prevalence': float,
-        'female_smokers': float,
-        'male_smokers': float,
-        'handwashing_facilities': float,
-        'hospital_beds_per_thousand': float,
-        'life_expectancy': float,
-        'human_development_index': float,
+        "iso_code": str,
+        "continent": str,
+        "location": str,
+        "total_cases": float,
+        "new_cases": float,
+        "new_cases_smoothed": float,
+        "total_deaths": float,
+        "new_deaths": float,
+        "new_deaths_smoothed": float,
+        "total_cases_per_million": float,
+        "new_cases_per_million": float,
+        "new_cases_smoothed_per_million": float,
+        "total_deaths_per_million": float,
+        "new_deaths_per_million": float,
+        "new_deaths_smoothed_per_million": float,
+        "total_tests": float,
+        "new_tests": float,
+        "total_tests_per_thousand": float,
+        "new_tests_per_thousand": float,
+        "new_tests_smoothed": float,
+        "new_tests_smoothed_per_thousand": float,
+        "tests_per_case": float,
+        "positive_rate": float,
+        "tests_units": str,
+        "stringency_index": float,
+        "population": float,
+        "population_density": float,
+        "median_age": float,
+        "aged_65_older": float,
+        "aged_70_older": float,
+        "gdp_per_capita": float,
+        "extreme_poverty": float,
+        "cardiovasc_death_rate": float,
+        "diabetes_prevalence": float,
+        "female_smokers": float,
+        "male_smokers": float,
+        "handwashing_facilities": float,
+        "hospital_beds_per_thousand": float,
+        "life_expectancy": float,
+        "human_development_index": float,
     }
     # url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
     # IN_DATA = "data/"
@@ -125,7 +125,11 @@ if __name__ == "__main__":
     # analysis_df = pd.read_csv(IN_DATA + file_name_data)
     country_codes = pd.read_csv(ppj("IN_DATA", "country_and_continent_codes.csv"))
 
-    analysis_df = pd.read_csv(ppj("IN_DATA_OWID", "owid-covid-data.csv"), dtype=owid_dtypes, parse_dates=["date"])
+    analysis_df = pd.read_csv(
+        ppj("IN_DATA_OWID", "owid-covid-data.csv"),
+        dtype=owid_dtypes,
+        parse_dates=["date"],
+    )
 
     # drop double entries
     country_codes.drop(
@@ -205,8 +209,7 @@ if __name__ == "__main__":
     analysis_df = analysis_df.sort_values(by=["iso_code", "date"])
 
     analysis_df.index = pd.MultiIndex.from_arrays(
-        [analysis_df.date, analysis_df.iso_code],
-        names=("date", "iso_code"),
+        [analysis_df.date, analysis_df.iso_code], names=("date", "iso_code"),
     )
     analysis_df.index = pd.MultiIndex.from_arrays(
         [
@@ -215,7 +218,9 @@ if __name__ == "__main__":
         ]
     )
 
-    analysis_df.loc[idx[:, "DEU"], "new_tests"] = analysis_df.loc[idx[:, "DEU"], "total_tests"].diff(periods=7)
+    analysis_df.loc[idx[:, "DEU"], "new_tests"] = analysis_df.loc[
+        idx[:, "DEU"], "total_tests"
+    ].diff(periods=7)
 
     analysis_df.loc[:, "new_cases_7d"] = analysis_df.new_cases.rolling(
         window=7, on=analysis_df.index.levels[0]
@@ -229,26 +234,37 @@ if __name__ == "__main__":
     )
 
     tmp_df = pd.merge(
-        analysis_df.new_cases.groupby([analysis_df.date.dt.isocalendar().week, analysis_df.iso_code]).sum(min_count=1),
-        analysis_df.new_tests.groupby([analysis_df.date.dt.isocalendar().week, analysis_df.iso_code]).sum(min_count=1),
+        analysis_df.new_cases.groupby(
+            [analysis_df.date.dt.isocalendar().week, analysis_df.iso_code]
+        ).sum(min_count=1),
+        analysis_df.new_tests.groupby(
+            [analysis_df.date.dt.isocalendar().week, analysis_df.iso_code]
+        ).sum(min_count=1),
         how="outer",
         left_index=True,
         right_index=True,
     )
 
-    tmp_df = tmp_df.rename(columns={"new_cases": "new_cases_week", "new_tests": "new_tests_week"})
+    tmp_df = tmp_df.rename(
+        columns={"new_cases": "new_cases_week", "new_tests": "new_tests_week"}
+    )
 
     analysis_df = pd.merge(
         analysis_df,
         tmp_df,
         how="left",
-        left_on=[analysis_df.date.dt.isocalendar().week.astype(str), analysis_df.iso_code.astype(str)],
-        right_on=[tmp_df.index.get_level_values(0).astype(str), tmp_df.index.get_level_values(1).astype(str)]
+        left_on=[
+            analysis_df.date.dt.isocalendar().week.astype(str),
+            analysis_df.iso_code.astype(str),
+        ],
+        right_on=[
+            tmp_df.index.get_level_values(0).astype(str),
+            tmp_df.index.get_level_values(1).astype(str),
+        ],
     )
 
     analysis_df.index = pd.MultiIndex.from_arrays(
-        [analysis_df.date, analysis_df.iso_code],
-        names=("date", "iso_code"),
+        [analysis_df.date, analysis_df.iso_code], names=("date", "iso_code"),
     )
     analysis_df.index = pd.MultiIndex.from_arrays(
         [
@@ -298,11 +314,17 @@ if __name__ == "__main__":
         analysis_df.new_tests_14d / analysis_df.population * 1e5
     )
 
-    analysis_df.loc[:, "positive_rate_7d"] = analysis_df.new_cases_7d / analysis_df.new_tests_7d
+    analysis_df.loc[:, "positive_rate_7d"] = (
+        analysis_df.new_cases_7d / analysis_df.new_tests_7d
+    )
 
-    analysis_df.loc[:, "positive_rate_14d"] = analysis_df.new_cases_14d / analysis_df.new_tests_14d
+    analysis_df.loc[:, "positive_rate_14d"] = (
+        analysis_df.new_cases_14d / analysis_df.new_tests_14d
+    )
 
-    analysis_df.loc[:, "positive_rate_week"] = analysis_df.new_cases_week / analysis_df.new_tests_week
+    analysis_df.loc[:, "positive_rate_week"] = (
+        analysis_df.new_cases_week / analysis_df.new_tests_week
+    )
 
     # countries = [
     #     "USA",
@@ -329,5 +351,3 @@ if __name__ == "__main__":
     for country in countries:
         country_data = analysis_df[analysis_df["iso_code"] == country]
         plot_development_over_time(country_data)
-
-    print("done")
--- src/utilities/sandbox.py
+++ src/utilities/sandbox.py
@@ -5,10 +5,8 @@ import os  # noqa:F401
 
 import matplotlib.dates as mdates  # noqa:F401
 import matplotlib.pyplot as plt  # noqa:F401
-
 import numpy as np  # noqa:F401
 import pandas as pd  # noqa:F401
-
 from scipy import interpolate  # noqa:F401
 
 from bld.project_paths import project_paths_join as ppj  # noqa:F401
@@ -31,7 +29,11 @@ from bld.project_paths import project_paths_join as ppj  # noqa:F401
 if __name__ == "__main__":
 
     file_list = os.listdir(ppj("IN_DATA", "divi"))
-    files = [file for file in file_list if file.startswith("DIVI-Intensivregister_") and file.endswith(".csv")]
+    files = [
+        file
+        for file in file_list
+        if file.startswith("DIVI-Intensivregister_") and file.endswith(".csv")
+    ]
 
     cols_analysis = [
         "bundesland",
@@ -42,20 +44,34 @@ if __name__ == "__main__":
         "anzahl_standorte",
         "betten_frei",
         "betten_belegt",
-        "daten_stand"
+        "daten_stand",
     ]
 
     analysis_df = pd.DataFrame(columns=cols_analysis)
 
     for file in files:
-        tmp_df = pd.read_csv(ppj("IN_DATA", "divi", file), dtype=float, parse_dates=["daten_stand"])
+        tmp_df = pd.read_csv(
+            ppj("IN_DATA", "divi", file), dtype=float, parse_dates=["daten_stand"]
+        )
         analysis_df = analysis_df.append(tmp_df)
 
     analysis_df.daten_stand = analysis_df.daten_stand.dt.date
-    df = analysis_df.groupby(["daten_stand"])[["faelle_covid_aktuell", "faelle_covid_aktuell_beatmet", "betten_frei", "betten_belegt"]].sum()
+    df = analysis_df.groupby(["daten_stand"])[
+        [
+            "faelle_covid_aktuell",
+            "faelle_covid_aktuell_beatmet",
+            "betten_frei",
+            "betten_belegt",
+        ]
+    ].sum()
 
-    df.rename(columns={"faelle_covid_aktuell": "total_cases_hospitalized", "faelle_covid_aktuell_beatmet": "total_cases_ventilated", "betten_frei": "beds_vacant", "betten_belegt": "beds_occupied"})
+    df.rename(
+        columns={
+            "faelle_covid_aktuell": "total_cases_hospitalized",
+            "faelle_covid_aktuell_beatmet": "total_cases_ventilated",
+            "betten_frei": "beds_vacant",
+            "betten_belegt": "beds_occupied",
+        }
+    )
 
-    print(analysis_df)
-
-
+    df.to_csv(ppj("IN_DATA", "divi_data.csv"))
